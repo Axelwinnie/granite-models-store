8e0c27ea4394d6c872b2b92db3689dcb9775c105
GMA <admin@granitemodels.local>
2026-04-12 20:48:41 -0400

    Shrink hero logo 420 to 260, tighten h1 margin, kill black void

---
+++ commit.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d C:\Users\Axelf\OneDrive\GRANITE-FINAL\deployed-store
+git add -A
+git commit -m "Shrink hero logo 420 to 260, tighten h1 margin, kill black void"
+git push origin main
